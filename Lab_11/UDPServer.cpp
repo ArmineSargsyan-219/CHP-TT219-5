33018ecd3bd56f79169b852782909fe1d1855df8
ArmineSargsyan-219 <arminesargsyan.tt219-2@polytechnic.am>
2024-12-09 12:01:54 +0400

    Update UDPServer.cpp

--- Lab_11/UDPServer.cpp
+++ Lab_11/UDPServer.cpp
@@ -1,1 +1,15 @@
+#include "UDPServer.h"
+
+UDPServer::UDPServer(unsigned short serverport) : UDPSocket(), mServerPort(serverport) {
+    mServer.sin_family = AF_INET;
+    mServer.sin_addr.s_addr = INADDR_ANY;
+    mServer.sin_port = htons(mServerPort);
+
+    if (bind(s, (struct sockaddr*)&mServer, sizeof(mServer)) == SOCKET_ERROR) {
+        printf("Bind failed with error code: %d", WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+}
+
+UDPServer::~UDPServer() {}
 
