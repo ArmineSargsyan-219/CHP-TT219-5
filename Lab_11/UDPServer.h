c3ea10aa771fdd87eaf3341799345cfb485d5344
ArmineSargsyan-219 <arminesargsyan.tt219-2@polytechnic.am>
2024-12-09 12:01:11 +0400

    Create UDPServer.h

---
+++ Lab_11/UDPServer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "UDPSocket.h"
+
+class UDPServer : public UDPSocket {
+private:
+    unsigned short mServerPort;
+    struct sockaddr_in mServer;
+    UDPServer();
+public:
+    UDPServer(unsigned short serverport);
+    virtual ~UDPServer();
+};
